453652eacd29ade26cf9ce95086be78780ac0e70
HeatherConnery <heatherconnery@gmail.com>
2015-10-22 12:01:40 -0400

    end of lecture

---
+++ ClassesAndStructsPart2/ClassesAndStructsPart2/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  ClassesAndStructsPart2
+//
+//  Created by Heather Connery on 2015-10-22.
+//  Copyright © 2015 HConnery. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        
+//    }
+//   
+
+}
--- ClassesAndStructsPart2/ClassesAndStructsPart2/ViewController.swift
+++ ClassesAndStructsPart2/ClassesAndStructsPart2/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //@IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func myButton(sender: AnyObject) {
+       // performSegueWithIdentifier("showSecondView", sender: self)
+        if myTextField.text == "hello" {
+            //call prepareForSegue function:
+            performSegueWithIdentifier("showSecondView", sender: self)
+        } else {
+            myLabel.text = "Bad Entry"
+            myLabel.textColor = UIColor.redColor()
+        }
+        
+    }
+    
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +37,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // for moving data.
+        if segue.identifier == "showSecondView" {
+            
+        }
+    }
+    
 
 
 }
